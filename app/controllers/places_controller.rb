e121ac7e6412db106d4d5caec29c353c63f25aab
octave collombel <octavec@outlook.com>
2020-08-17 17:27:40 +0200

    added search function based on income and changed the seed file a little

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,15 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all
+    if params[:query].present?
+      @places = Place.where(min_income: (0..params[:query].to_i))
+      # or you can also use:
+      # @places = Place.where("min_income <= ?", params[:query])
+      @places = "Fuck off peasant" if @places.empty?
+    else
+      @places = Place.all
+    end
   end
+
   def show
     @place = Place.find(params[:id])
   end
