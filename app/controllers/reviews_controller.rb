b1e340820b4f371b95c90d7b399c60e9d0a07d23
corentin bourdat <corentinbourdat@laposte.net>
2020-08-20 13:14:37 +0200

    redirect fixé

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,20 @@
 class ReviewsController < ApplicationController
-
   def create
     review = Review.new(strong_review)
     review.user = current_user
     review.place = Place.find(params[:place_id])
+    redirect_to place_path(param[:place_id])
   end
 
   def destroy
     Review.destroy(Review.find(params[:id]))
+    redirect_to place_path(param[:place_id])
   end
 
   private
 
   def strong_review
     params.require(:review).permit(:content, :rating)
+    re
   end
 end
