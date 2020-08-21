9fda2eb7ce3bc85e91b060787d7e9dd2c98caad1
corentin bourdat <corentinbourdat@laposte.net>
2020-08-21 11:53:16 +0200

    ??

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2020_08_21_093503) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
